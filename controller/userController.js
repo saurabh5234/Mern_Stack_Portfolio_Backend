1ef91f82a62b63c8ea96fd89add4d8cd50fcad09
saurabh <saurabhmali1372002@gmail.com>
2025-08-24 18:43:18 +0530

    make changes in userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -3,7 +3,7 @@ import ErrorHandler from "../middlewares/error.js";
 import { User } from "../models/userSchema.js";
 import {v2 as cloudinary} from "cloudinary";
 import { generateToken } from "../utils/jwtToken.js";
-import { sendEmail } from "../utils/sendemail.js";
+import { sendEmail } from "../utils/sendEmail.js";
 import crypto from "crypto";
 
 // Handle file upload and user registration logic here
